d9aacb186670d65d7f9220d6d172c275a09c7764
Will Adams-Keane <WAK@new-host.home>
2013-09-29 18:16:42 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrinksRemixed::Application.config.secret_key_base = 'f5e73676f39d079b23c9b22380785296ec474e5f6e6318f2ae88f1f5dc0ba27951ebbb0acde202755eeaf3b4ab5e9b3219c2f30f24987ae63834bde445ef846a'
